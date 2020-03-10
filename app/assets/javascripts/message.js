48ca9e31ea933f1ea6e8c058c70f069645c1fb77
ryjtkd1204 <61186866+ryjtkd1204@users.noreply.github.com>
2020-03-10 17:21:49 +0900

    変更しました

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,48 +2,48 @@ $(function(){
   var buildHTML = function(message) {
     if (message.content && message.image) {
       //data-idが反映されるようにしている
-      var html = `<div class="message" data-message-id=  message.id  >
+      var html = `<div class="message" data-message-id=  ${message.id} >
         <div class="upper-message">
           <div class="upper-message__user-name">
-            message.user_name
+            ${message.user_name}
           </div>
           <div class="upper-message__date">
-            message.created_at 
+            ${message.created_at}
           </div>
         </div>
         <div class="lower-message">
           <p class="lower-message__content">
-            message.content 
+            ${message.content} 
           </p>
           <img src=" message.image " class="lower-message__image" >
         </div>
       </div>`
     } else if (message.content) {
       //同様に、data-idが反映されるようにしている
-      var html = `<div class="message" data-message-id= message.id >
+      var html = `<div class="message" data-message-id= ${message.id} >
         <div class="upper-message">
           <div class="upper-message__user-name">
-            message.user_name 
+            ${message.user_name} 
           </div>
           <div class="upper-message__date">
-            message.created_at 
+            ${message.created_at} 
           </div>
         </div>
         <div class="lower-message">
           <p class="lower-message__content">
-            message.content 
+            ${message.content} 
           </p>
         </div>
       </div>`
     } else if (message.image) {
       //同様に、data-idが反映されるようにしている
-      var html = `<div class="message" data-message-id= message.id >
+      var html = `<div class="message" data-message-id= ${message.id} >
         <div class="upper-message">
           <div class="upper-message__user-name">
-            message.user_name 
+            ${message.user_name}
           </div>
           <div class="upper-message__date">
-            message.created_at 
+            ${message.created_at} 
           </div>
         </div>
         <div class="lower-message">
